acc67bdf88449b153feeaa78116612e75eabf38b
Efraim Cancio <efraimfonseca@gmail.com>
2023-04-20 21:04:40 -0300

    criando o hook useThemeSwitcher

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,14 +29,14 @@ export default function Home() {
             </div>
             <div className="w-1/2 flex flex-col items-center self-center">
               <AnimatedText
-                text="Turning Vision Into Reality With Code And Design."
+                text="Transformando a sua visão em realidade com código e design."
                 className="!text-6xl text-left"
               />
               <p className="my-4 text-base font-medium">
-                As a skilled full-stack developer, I am dedicated to turning
-                ideas into innovative web applications. Explore my latest
-                projects and articles, showcasing my expertise in React.js and
-                web development.
+                Como um habilidoso desenvolvedor full-stack, estou dedicado a
+                transformar ideias em aplicações web inovadoras. Expor meus mais
+                recente projetos e artigos, mostrando minha experiência em
+                React.js e desenvolvimento web.
               </p>
               <div className="flex items-center self-start mt-2">
                 <Link
